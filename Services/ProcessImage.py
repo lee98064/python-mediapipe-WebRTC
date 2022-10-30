62609502b5695ce977c6eb3edac3322f24678514
Ryan Lee <lee98064@gmail.com>
2022-10-30 22:49:09 +0800

    git init

---
+++ Services/ProcessImage.py
@@ -0,0 +1,127 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+
+class ProcessImage():
+
+    def __init__(self):
+        self.counter = 0
+        self.stage = ""
+
+    def calculate_angle(self, a, b, c):
+        a = np.array(a)  # First
+        b = np.array(b)  # Mid
+        c = np.array(c)  # End
+
+        radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - \
+            np.arctan2(a[1]-b[1], a[0]-b[0])
+        angle = np.abs(radians*180.0/np.pi)
+
+        # cy - by, cx-bx
+
+        if angle > 180.0:
+            angle = 360-angle
+
+        return angle
+
+    def process_frame(self, frame):
+
+        with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+
+            # Make detection
+            results = pose.process(image)
+
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+
+                # Get coordinates
+
+                # 左跨部
+                l_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+                         landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+
+                # 左膝蓋
+                l_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
+                          landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+
+                # 左腳踝
+                l_ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+                           landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
+
+                # 左腳跟
+                l_heel = [landmarks[mp_pose.PoseLandmark.LEFT_HEEL.value].x,
+                          landmarks[mp_pose.PoseLandmark.LEFT_HEEL.value].y]
+
+                # 左腳前
+                l_foot_index = [landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].x,
+                                landmarks[mp_pose.PoseLandmark.LEFT_FOOT_INDEX.value].y]
+
+                # Calculate angle
+                angle = self.calculate_angle(l_hip, l_knee, l_ankle)
+
+                ankleAngle = self.calculate_angle(
+                    l_knee, l_ankle, l_foot_index)
+
+                # Visualize angle
+                cv2.putText(image, str(angle),
+                            tuple(np.multiply(l_knee, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,
+                                                            255, 255), 2, cv2.LINE_AA
+                            )
+
+                # Visualize angle
+                cv2.putText(image, str(ankleAngle),
+                            tuple(np.multiply(l_heel, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,
+                                                            255, 255), 2, cv2.LINE_AA
+                            )
+
+                # Curl counter logic
+                if angle > 100:
+                    self.stage = "d"
+                if angle < 100 and self.stage == 'd':
+                    self.stage = "up"
+                    self.counter += 1
+
+            except:
+                pass
+
+            # Render curl counter
+            # Setup status box
+            cv2.rectangle(image, (0, 0), (700, 73), (245, 117, 16), -1)
+
+            # Rep data
+            cv2.putText(image, 'REPS', (15, 12),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, str(self.counter),
+                        (10, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Stage data
+            cv2.putText(image, 'STAGE', (65, 12),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(image, self.stage,
+                        (60, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Render detections
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                      mp_drawing.DrawingSpec(
+                                          color=(245, 117, 66), thickness=2, circle_radius=2),
+                                      mp_drawing.DrawingSpec(
+                                          color=(245, 66, 230), thickness=2, circle_radius=2)
+                                      )
+
+        return image
---
+++ main.py
@@ -0,0 +1,135 @@
+import argparse
+import asyncio
+from concurrent.futures import process
+import json
+import logging
+import os
+import platform
+import ssl
+import time
+
+import cv2
+from aiohttp import web
+from aiortc import (
+    MediaStreamTrack,
+    RTCDataChannel,
+    RTCPeerConnection,
+    RTCSessionDescription,
+    VideoStreamTrack,
+)
+from aiortc.contrib.media import MediaPlayer, MediaRelay
+from av import VideoFrame
+from Services.ProcessImage import ProcessImage
+
+ROOT = os.path.dirname(__file__)
+
+
+relay = None
+webcam = None
+
+
+async def index(request):
+    content = open(os.path.join(ROOT, "index.html"), "r").read()
+    return web.Response(content_type="text/html", text=content)
+
+
+async def javascript(request):
+    content = open(os.path.join(ROOT, "js/client.js"), "r").read()
+    return web.Response(content_type="application/javascript", text=content)
+
+
+async def offer(request):
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    pc = RTCPeerConnection()
+    pcs.add(pc)
+
+    await server(pc, offer)
+
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps(
+            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+        ),
+    )
+
+
+pcs = set()
+
+
+async def server(pc, offer):
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        print("Connection state is %s" % pc.connectionState)
+        if pc.connectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
+
+    @pc.on("track")
+    def on_track(track):
+        print("======= received track: ", track)
+        if track.kind == "video":
+            t = FaceSwapper(track)
+            pc.addTrack(t)
+
+    await pc.setRemoteDescription(offer)
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+
+async def on_shutdown(app):
+    # close peer connections
+    coros = [pc.close() for pc in pcs]
+    await asyncio.gather(*coros)
+    pcs.clear()
+
+
+class FaceSwapper(VideoStreamTrack):
+    kind = "video"
+
+    def __init__(self, track):
+        super().__init__()
+        self.track = track
+        self.processImage = ProcessImage()
+        # self.face_detector = cv2.CascadeClassifier(
+        #     "./haarcascade_frontalface_alt.xml")
+        # self.face = cv2.imread("./wu.png")
+
+    async def recv(self):
+        timestamp, video_timestamp_base = await self.next_timestamp()
+        frame = await self.track.recv()
+        frame = frame.to_ndarray(format="bgr24")
+        frame = self.processImage.process_frame(frame)
+
+        # s = time.time()
+        # face_zones = self.face_detector.detectMultiScale(
+        #     cv2.cvtColor(frame, code=cv2.COLOR_BGR2GRAY)
+        # )
+        # for x, y, w, h in face_zones:
+        #     face = cv2.resize(self.face, dsize=(w, h))
+        #     frame[y : y + h, x : x + w] = face
+        frame = VideoFrame.from_ndarray(frame, format="bgr24")
+        frame.pts = timestamp
+        frame.time_base = video_timestamp_base
+        return frame
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="WebRTC webcam demo")
+    parser.add_argument(
+        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
+    )
+    parser.add_argument(
+        "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO)
+
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+    app.router.add_get("/", index)
+    app.router.add_get("/js/client.js", javascript)
+    app.router.add_post("/offer", offer)
+    web.run_app(app, host=args.host, port=args.port)
