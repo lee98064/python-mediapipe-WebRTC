49853c6ada0e5852e6000517a9dee5b2d928c407
Ryan Lee <lee98064@gmail.com>
2022-10-31 00:58:04 +0800

    Add frontend ws code

--- js/client.js
+++ js/client.js
@@ -2,6 +2,9 @@ var pc = null;
 var localVideo = document.querySelector("video#localVideo");
 var serverVideo = document.querySelector("video#serverVideo");
 
+var room = generateRandomString(5);
+var user_name = generateRandomString(5);
+
 navigator.mediaDevices
   .getUserMedia({
     video: {
@@ -41,7 +44,7 @@ function negotiate() {
     })
     .then(function () {
       var offer = pc.localDescription;
-      return fetch("/offer", {
+      return fetch(`/offer?room=${room}&user_name=${user_name}`, {
         body: JSON.stringify({
           sdp: offer.sdp,
           type: offer.type,
@@ -81,9 +84,22 @@ function start() {
     }
   });
 
+  wsStart();
   document.getElementById("start").style.display = "none";
   negotiate();
   document.getElementById("stop").style.display = "inline-block";
+}
+
+function generateRandomString(num) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result1 = "";
+  const charactersLength = characters.length;
+  for (let i = 0; i < num; i++) {
+    result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result1;
 }
 
 function stop() {
@@ -94,4 +110,38 @@ function stop() {
   }, 500);
 }
 
-function wsStart() {}
+function wsStart() {
+  let socket = new WebSocket(
+    `ws://localhost:8080/ws?room=${room}&user_name=${user_name}`
+  );
+
+  socket.onopen = function (e) {
+    console.log("[open] Connection established");
+    // console.log("Sending to server");
+    // socket.send("My name is John");
+  };
+
+  socket.onmessage = function (event) {
+    const data = JSON.parse(event.data);
+    document.querySelector("#information").innerHTML = `做了：${
+      data.counter
+    } 下，現在你的腳：${data.stage == "up" ? "抬起" : "放下"}`;
+    // alert(`[message] Data received from server: ${event.data}`);
+  };
+
+  socket.onclose = function (event) {
+    if (event.wasClean) {
+      alert(
+        `[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`
+      );
+    } else {
+      // 例如服务器进程被杀死或网络中断
+      // 在这种情况下，event.code 通常为 1006
+      alert("[close] Connection died");
+    }
+  };
+
+  socket.onerror = function (error) {
+    alert(`[error] ${error.message}`);
+  };
+}
